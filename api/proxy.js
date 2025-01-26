3a85ca596f668fcdc9ef0657c54c38047e3e5600
boygemini <gbadeojubril@gmail.com>
2025-01-26 08:14:49 -0800

    nc

--- api/proxy.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch("http://ip-api.com/json/");
-  const data = await response.json();
-
-  res.status(200).json(data);
-}
